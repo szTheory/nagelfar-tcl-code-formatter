ba7ff3613453cfe6318d9dbc358b8ac31e37523e
Peter Spjuth <peter.spjuth@gmail.com>
2018-10-20 21:47:23 +0200

    Do not check vars during header generation.

--- src/startup.tcl
+++ src/startup.tcl
@@ -293,6 +293,7 @@ if {![info exists gurka]} {
                     warnShortSub 0
                     strictAppend 0
                     forceElse 0
+                    noVar 1
                     severity E
                 }
             }
